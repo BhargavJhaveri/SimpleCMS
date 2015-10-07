92ae57a5abd939daa4f247ee5e4e37d606233c3a
Bhargav R. Jhaveri <bhargavjhaveri@gmail.com>
2015-10-07 18:20:19 -0400

    Minor changes

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,6 +1,18 @@
 class DemoController < ApplicationController
-  # def index
-  # 	render('hello')
-  # end
+  def index
+  	render('index')
+  end
   
+  def hello
+  	# render('hello')
+  	@array = [1,2,3,4,5]
+  end
+
+  def other_hello
+  	redirect_to(:action => 'index')
+  end
+
+  def lynda
+  	redirect_to('http://lynda.com')
+  end
 end
